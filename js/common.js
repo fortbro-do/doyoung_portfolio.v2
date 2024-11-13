47995b15df0bf378ab8a145287a87b42e4924344
fortbro-do <ehdud1223@gmail.com>
2024-11-13 17:14:23 +0900

    레퍼지토리 생성

---
+++ js/common.js
@@ -0,0 +1,8 @@
+import { gsap } from "gsap";
+    
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { Observer } from "gsap/Observer";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+
+
+gsap.registerPlugin(ScrollTrigger,Observer,ScrollToPlugin);
