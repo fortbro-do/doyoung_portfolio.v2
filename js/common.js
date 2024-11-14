a29ff590377103a1d4e24afe587b8f40d70503b0
fortbro-do <ehdud1223@gmail.com>
2024-11-15 08:42:39 +0900

    home 수정 및 resume 추가

--- js/common.js
+++ js/common.js
@@ -1,8 +1,6 @@
 import { gsap } from "gsap";
-    
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { Observer } from "gsap/Observer";
 import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 
-
-gsap.registerPlugin(ScrollTrigger,Observer,ScrollToPlugin);
+gsap.registerPlugin(ScrollTrigger, Observer, ScrollToPlugin);
